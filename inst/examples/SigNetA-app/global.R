6a9a33d8e2f621e027ac7055cd53e42a642abebe
karimrn <karimrn@mail.uc.edu>
2018-09-07 13:37:12 -0400

    .rda files extracted from inside docker container

--- inst/examples/SigNetA-app/global.R
+++ inst/examples/SigNetA-app/global.R
@@ -26,9 +26,13 @@ edgeGO<<-NULL #visobject edge with GO terms in it
 ##Load STRING Data##
 
 
-load(url("http://www.ilincs.org/tmp/signeta/weightedGraphStringPPI_10.rda")) #STRING PPI network
-load(url("http://www.ilincs.org/tmp/signeta/stringToEntrezDetailed.Rdata")) #edge data
-load( url("http://www.ilincs.org/tmp/signeta/lincscp_1.rda"))
+# load(url("http://www.ilincs.org/tmp/signeta/weightedGraphStringPPI_10.rda")) #STRING PPI network
+# load(url("http://www.ilincs.org/tmp/signeta/stringToEntrezDetailed.Rdata")) #edge data
+# load( url("http://www.ilincs.org/tmp/signeta/lincscp_1.rda"))
+
+load("/tmp/datasets/weightedGraphStringPPI_10.rda") #STRING PPI network
+load("/tmp/datasets/stringToEntrezDetailed.Rdata") #edge data
+load( "/tmp/datasets/lincscp_1.rda")
 
 
 ##END..Load STRING Data##
