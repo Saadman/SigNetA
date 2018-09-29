4754f70727168895c3e2df9aacfb1fbc3104b4ad
karimrn <karimrn@mail.uc.edu>
2018-09-29 04:15:39 -0400

    gene names from geneData.rda

--- inst/examples/SigNetA-app/global.R
+++ inst/examples/SigNetA-app/global.R
@@ -29,16 +29,16 @@ edgeGO<<-NULL #visobject edge with GO terms in it
 
 #load(url("http://www.ilincs.org/tmp/signeta/weightedGraphStringPPI_10.rda")) #STRING PPI network
 #load(url("http://www.ilincs.org/tmp/signeta/stringToEntrezDetailed.Rdata")) #edge data
-load( url("http://www.ilincs.org/tmp/signeta/lincscp_1.rda"))
-load("/Users/Rashid/Desktop/Rpackages/SigNetA_Data/weightedGraphStringPPI_10.rda")#new STRING PPI network
-load("/Users/Rashid/Desktop/Rpackages/SigNetA_Data/interactome.rda")
-load("/Users/Rashid/Desktop/Rpackages/SigNetA_Data/geneData.rda")
+# load( url("http://www.ilincs.org/tmp/signeta/lincscp_1.rda"))
+# load("/Users/Rashid/Desktop/Rpackages/SigNetA_Data/weightedGraphStringPPI_10.rda")#new STRING PPI network
+# load("/Users/Rashid/Desktop/Rpackages/SigNetA_Data/interactome.rda")
+# load("/Users/Rashid/Desktop/Rpackages/SigNetA_Data/geneData.rda")
 
-# load("/tmp/datasets/weightedGraphStringPPI_10.rda") #STRING PPI network
-# #load("/tmp/datasets/stringToEntrezDetailed.rda") #edge data
-# load("/tmp/datasets/interactome.rda")#interactome
-# load( "/tmp/datasets/lincscp_1.rda")
-# load("/tmp/datasets/geneData.rda")
+load("/tmp/datasets/weightedGraphStringPPI_10.rda") #STRING PPI network
+#load("/tmp/datasets/stringToEntrezDetailed.rda") #edge data
+load("/tmp/datasets/interactome.rda")#interactome
+load( "/tmp/datasets/lincscp_1.rda")
+load("/tmp/datasets/geneData.rda")
 
 
 
